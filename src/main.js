e35f27bfdabd1c4eaea8f4c77cf701498365959d
Christian Svensson <christian.svensson@apptus.com>
2016-12-10 00:05:07 +0100

    added dynamic canvas/image resize

--- src/main.js
+++ src/main.js
@@ -1,29 +1,53 @@
 document.addEventListener('DOMContentLoaded', init);
 
 
+const IMAGE_X = 'imageX';
+const IMAGE_Y = 'imageY';
+const IMAGE_ANCHOR = 'imageAnchor';
+const CANVAS_X = 'canvasX';
+const CANVAS_Y = 'canvasY';
+const CANVAS_ANCHOR = 'canvasAnchor';
+
+let currentImage;
+
 function init() {
+  on('#select', 'click', () => qs('#file').click());
+  on('#controls', 'input', redrawCanvas);
   on('#file', 'change', loadImage);
-  on('#dropzone', 'dragover', e => e.preventDefault());
-  on('#dropzone', 'dragenter', classToggleFactory(true, 'dragging'));
-  on('#dropzone', 'dragleave', classToggleFactory(false, 'dragging'));
-  on('#dropzone', 'drop', classToggleFactory(false, 'dragging'));
-  on('#dropzone', 'drop', fileDrop);
+  on('html', 'dragover', dragOver);
+  on('html', 'drop', fileDrop);
 }
 
 function on(selector, eventType, handler) {
-  const elem = document.querySelector(selector);
-
-  elem.addEventListener(eventType, handler);
+  qs(selector).addEventListener(eventType, handler);
 
   return () => {
-    elem.removeEventListener(eventType, handler);
+    qs(selector).removeEventListener(eventType, handler);
   };
 }
 
-function classToggleFactory(add, className) {
-  return function() {
-    this.classList.toggle(className, add);
-  };
+function qs(selector) {
+  return document.querySelector(selector);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+
+  const overlay = qs('.overlay.dragging');
+  overlay.classList.add('active');
+
+  if (dragOver.timer) clearTimeout(dragOver.timer);
+
+  dragOver.timer = setTimeout(() => {
+    dragOver.timer = null;
+
+    const cl = overlay.classList;
+
+    cl.remove('active');
+    cl.add('active-remove');
+
+    setTimeout(() => cl.remove('active-remove'), 400);
+  }, 200);
 }
 
 function fileDrop(e) {
@@ -32,18 +56,63 @@ function fileDrop(e) {
 }
 
 function loadImage() {
-  const img = document.createElement('img');
-  img.src = window.URL.createObjectURL(this.files[0]);
-  img.addEventListener('load', loadCanvas);
+  const img = currentImage = document.createElement('img');
 
-  document.querySelector('#img').src = img.src;
+  img.src = window.URL.createObjectURL(this.files[0]);
+  img.addEventListener('load', updateControls);
 }
 
-function loadCanvas() {
-  const canvas = document.querySelector('#canvas');
-  const canvasPad = 100;
+function updateControls() {
+  const controls = qs('#controls');
+  controls[IMAGE_X].value = this.width;
+  controls[IMAGE_Y].value = this.height;
+  controls[CANVAS_X].value = this.width;
+  controls[CANVAS_Y].value = this.height;
 
-  canvas.width = this.width + canvasPad;
-  canvas.height = this.height + canvasPad;
-  canvas.getContext('2d').drawImage(this, canvasPad / 2, canvasPad / 2);
+  redrawCanvas(this);
+}
+
+function redrawCanvas() {
+  if (!currentImage) return;
+
+  const img = currentImage;
+  const d = new FormData(qs('#controls'));
+  const canvas = document.querySelector('#canvas');
+
+  const width = canvas.width = d.get(CANVAS_X);
+  const height = canvas.height = d.get(CANVAS_Y);
+  const resizeX = d.get(IMAGE_X);
+  const resizeY = d.get(IMAGE_Y);
+
+  const { x, y } = offset(d.get(CANVAS_ANCHOR), { width, height }, img);
+  const { x: x2, y: y2 } = offset(d.get(IMAGE_ANCHOR), img, { width: resizeX, height: resizeY });
+  console.log(x, x2, y, y2);
+
+  canvas.getContext('2d').drawImage(img, x + x2, y + y2, resizeX, resizeY);
+}
+
+function offset([xPos, yPos], { width: w, height: h }, { width: w2, height: h2 }) {
+  let x = 0;
+  let y = 0;
+
+  if (xPos === 'c') {
+    x += (w - w2) / 2;
+  } else if (xPos === 'r') {
+    x += w - w2;
+  }
+
+  if (yPos === 'c') {
+    y += (h - h2) / 2;
+  } else if (yPos === 'b') {
+    y += h - h2;
+  }
+
+  return { x, y };
+}
+
+function raf(next, ...rest) {
+  requestAnimationFrame(() => {
+    next();
+    if (rest.length) raf(...rest);
+  });
 }
